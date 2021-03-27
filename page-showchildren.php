3f2ccf3c2b0b8e0810d7bea0cae639d1b2052794
Tyler Finck <tyler@Tylers-MacBook-Air.local>
2021-03-27 13:26:22 -0400

    more updates for block support

--- page-showchildren.php
+++ page-showchildren.php
@@ -1,23 +1,22 @@
 <?php
 /*
- * 
+ *
  Template Name: Show Children
  */
 
-get_header(); 
+get_header();
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			
+
 
 			<?php while ( have_posts() ) : the_post(); ?>
 <?php the_title( '<h1 id="entry-title">', '</h1>' ); ?>
-			<?php the_post_thumbnail( 'large' ); ?> 
 			<?php the_content(); ?>
 
-				
+
 
 				<ul id="children">
 				<?php
@@ -34,13 +33,13 @@ get_header();
 				$parent = new WP_Query( $args );
 
 				if ( $parent->have_posts() ) : ?>
-				
+
 				    <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
 					    <?php
 					$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 300,300 ), false, '' );
 					?>
 				        <li id="parent-<?php the_ID(); ?>" class="parent-page" style="background-image: url(<?php echo $src[0]; ?>);">
-							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"> 
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 				            <h2><?php the_title(); ?></h2></a>
 
 				        </li>
@@ -57,12 +56,12 @@ get_header();
 
 
 			<?php endwhile; // end of the loop. ?>
-		
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 
-	
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
